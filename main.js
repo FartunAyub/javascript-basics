6eafb1bd1972d1c516d7429d9d3894b6332a161e
FartunAyub <fartun.hassan@students.jkuat.ac.ke>
2024-07-30 13:23:01 +0300

    Add JavaScript code for data types

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // JavaScript code will go here
 console.log("JavaScript Basics");
+// Part 1: Data Types
+
+// 1. Create variables of different data types
+let stringVar = "This is a string"; // String
+let numberVar = 42; // Number
+let booleanVar = true; // Boolean
+let nullVar = null; // Null
+let undefinedVar; // Undefined
+let objectVar = { name: "John", age: 30 }; // Object
+let symbolVar = Symbol("description"); // Symbol
+
+// 2. Print these variables to the console using console.log()
+console.log("String:", stringVar);
+console.log("Number:", numberVar);
+console.log("Boolean:", booleanVar);
+console.log("Null:", nullVar);
+console.log("Undefined:", undefinedVar);
+console.log("Object:", objectVar);
+console.log("Symbol:", symbolVar);
